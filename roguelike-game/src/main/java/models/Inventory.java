8c0ab421513cd6fa6f080ed868d62bf888d411f3
Nickkreker <nick_berezikov@me.com>
2022-11-15 18:58:36 +0300

    Write some missing documentation

--- roguelike-game/src/main/java/models/Inventory.java
+++ roguelike-game/src/main/java/models/Inventory.java
@@ -20,18 +20,33 @@ public class Inventory {
         return instance;
     }
 
+    /**
+     * Add item to the inventory.
+     * @param item Item ro be added
+     */
     public void addItem(Item item) {
         items.add(item);
     }
 
+    /**
+     * Gets list of item that currently in the inventory.
+     *
+     * @return List of items the inventory contains
+     */
     public List<Item> getItems() {
         return items;
     }
 
+    /**
+     * Select next item in the inventory.
+     */
     public void selectNext() {
         lastSelectedItemId = (lastSelectedItemId + 1) % items.size();
     }
 
+    /**
+     * Select previous item in the inventory.
+     */
     public void selectPrevious() {
         if (lastSelectedItemId == 0)
             lastSelectedItemId = items.size() - 1;
@@ -39,24 +54,49 @@ public class Inventory {
             lastSelectedItemId -= 1;
     }
 
+    /**
+     * Set the select status for the inventory. If someone wants to equip an item, he/she should first select it.
+     *
+     * @param status boolean value to which the internal select status will be set
+     */
     public void setSelectedStatus(boolean status) {
         selected = status;
     }
 
+    /**
+     * Checks whether any item is selected.
+     *
+     * @return true if there is any selected item in the inventory, false - otherwise
+     */
     public boolean isSelected() {
         return selected;
     }
 
+    /**
+     * Gets the id of the last selected item.
+     *
+     * @return Id of the last selected item
+     */
     public int getSelectedId() {
         return lastSelectedItemId;
     }
 
+    /**
+     * Change the equipped status of the currently selected item.
+     *
+     * @param player Player whose characteristics will be altered after the equipped status of item is changed
+     */
     public void selectedItemChangeEquipStatus(Player player) {
         if (!selected)
             return;
         items.get(lastSelectedItemId).changeEquipStatus(player);
     }
 
+    /**
+     * Gets description of the currently selected item.
+     *
+     * @return String description of an item
+     */
     public String getDescription() {
         return "Item description";
     }
