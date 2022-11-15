c4e676d22c6402676bf261cbae3f045290bf5916
yaroslav2k <iaroslav2k@gmail.com>
2022-11-15 21:36:11 +0300

    Provide basic specs and implement CI

---
+++ roguelike-game/src/test/java/lib/EventTest.java
@@ -0,0 +1,15 @@
+package test.lib;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import lib.Event;
+
+class EventTest {
+    @Test
+    public void testGetType() {
+        Event event = new Event(Event.Type.GAME_EXIT);
+
+        assertEquals(event.getType(), Event.Type.GAME_EXIT);
+    }
+}
