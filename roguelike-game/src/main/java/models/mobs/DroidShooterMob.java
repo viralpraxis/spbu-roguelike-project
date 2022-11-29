e4aa188e4bef3d29a2356050915331ad89c29c5f
Nickkreker <nick_berezikov@me.com>
2022-11-29 16:38:44 +0300

    Add ClonableMod interface + make DroidShooterMob clonable

--- roguelike-game/src/main/java/models/mobs/DroidShooterMob.java
+++ roguelike-game/src/main/java/models/mobs/DroidShooterMob.java
@@ -1,11 +1,27 @@
 package models.mobs;
 
-
 /**
  * Concrete descendant of WeakMob class that is used by science-fiction factory.
  */
-public class DroidShooterMob extends WeakMob {
+public class DroidShooterMob extends WeakMob implements ClonableMob {
+    private float replicationProb = 0.1f;
+
     public DroidShooterMob(int posX, int posY) {
         super(posX, posY, 121, 40);
+    }
+
+    private DroidShooterMob(int posX, int posY, int health, int strength) {
+        super(posX, posY, health, strength);
+    }
+
+    @Override
+    public DroidShooterMob clone() {
+        var copyMob = new DroidShooterMob(this.posX(), this.posY(), this.getHealth(), this.getPower());
+        return copyMob;
+    }
+
+    @Override
+    public float getReplicationProb() {
+        return replicationProb;
     }
 }
