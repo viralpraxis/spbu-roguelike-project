aaf235f019956bf56913229a93474e0c30f6e953
yaroslav2k <iaroslav2k@gmail.com>
2022-12-05 23:59:50 +0300

    Let GameController not be connected with BL classes directly

--- roguelike-game/src/main/java/commands/ChangeSelectedItemEquipStatusCommand.java
+++ roguelike-game/src/main/java/commands/ChangeSelectedItemEquipStatusCommand.java
@@ -1,17 +1,14 @@
 package commands;
 
+import core.GameInitializer;
 import models.Inventory;
 import models.mobs.Player;
 
 public class ChangeSelectedItemEquipStatusCommand implements ICommand {
-    private Player player;
-
-    public ChangeSelectedItemEquipStatusCommand(Player player) {
-        this.player = player;
-    }
-
     @Override
     public void execute() {
-        Inventory.getInventory().selectedItemChangeEquipStatus(player);
+        Inventory.getInventory().selectedItemChangeEquipStatus(
+            GameInitializer.getGameState().getPlayer()
+        );
     }
 }
